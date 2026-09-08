04e7c06906868403613eccad5924c20b0ab18553
AhmedQandeell <59745955+AhmedQandeell@users.noreply.github.com>
2026-09-08 16:30:34 +0300

    Correct Ashrqat spelling in metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,9 +3,9 @@ import MusicPlayer from "./music-player";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "Ahmed & Ashraqat | Wedding Invitation",
+  title: "Ahmed & Ashrqat | Wedding Invitation",
   description:
-    "Join us to celebrate the wedding of Ahmed Qandeel and Ashraqat El-Bidwehy. Friday, 9 October 2026, 8 PM at Tiba Rose Hotel.",
+    "Join us to celebrate the wedding of Ahmed Qandeel and Ashrqat El-Bidwehy. Friday, 9 October 2026, 8 PM at Tiba Rose Hotel.",
   robots: { index: false, follow: false },
   icons: { icon: "/favicon.svg" },
 };
